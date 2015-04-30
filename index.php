f69c32f8ee4a39850436e73bd37a25180bd87376
sliktrik <kyle@sliktrik.com.au>
2015-04-30 12:19:52 +1000

    wscs

--- index.php
+++ index.php
@@ -54,6 +54,7 @@
         <a href="tel:0422313908">0422 313 908</a>
     </div>
     <canvas id="canvas"width="400" height="400"></canvas>
+    <div class="coming">website coming soon</div>
     <div id="particles-js"></div>
     <script src="js/lib/particles.min.js"></script>
     <script src="js/app.js"></script>
